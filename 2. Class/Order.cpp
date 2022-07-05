f07daa17d00762c9a4d36d1b5f1665ce2ff742de
YangJunMan <tfea21@naver.com>
2022-07-05 22:31:37 +0900

    [C++] 2.Class 클래스의 존재는 C에서 C++로 넘어가는 큰 이유중 하나이며, 코드를 재사용하기 쉽게 해주고 코드의 가독성 또한 높여주기에 클래스의 기능을 이해해야합니다. 구조체처럼 코드를 더욱 간결하게 확인할 수 있도록 해주고, 재사용성 또한 높여주기에 예제를 살펴보도록 합니다.

---
+++ 2. Class/Order.cpp
@@ -0,0 +1,10 @@
+#include "Order.h"
+#include <iostream>
+
+int main() {
+	order A;
+
+	A.insert();
+	A.crank();
+
+}
---
+++ 2. Class/Sum.cpp
@@ -0,0 +1,11 @@
+#include "Sum.h"
+#include <iostream>
+
+int main() {
+	Sum_Matrix A;
+
+	A.Sum_Row();
+	A.Sum_Col();
+	A.Print_Matrix();
+	return 0;
+}
